f0bbfe74aae348def7b8f66fdd9d6c607a427056
Andrew <arhorn@smcm.edu>
2019-03-12 00:48:06 -0500

    added memoizedFibonacci.js

---
+++ 5 kyu/memoizedFibonacci.js
@@ -0,0 +1,18 @@
+let memoized = [0, 1];
+
+const fibonacci = n => {
+  if (n < memoized.length) {
+    return memoized[n];
+  };
+  const result = fibonacci(n - 1) + fibonacci(n - 2);
+  memoized.push(result);
+  return result;
+};
+
+const assert = require('assert');
+console.log(fibonacci(20));
+console.log(fibonacci(40));
+
+assert.equal(fibonacci(70), 190392490709135, 'test 1');
+assert.equal(fibonacci(60), 1548008755920, 'test 2');
+assert.equal(fibonacci(50), 12586269025, 'test 3');
